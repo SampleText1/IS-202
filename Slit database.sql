a58d966977b75fe0a14a02e0c7c6ef567fd0381c
PolyDeucs <marius_kaurin@hotmail.no>
2017-10-23 14:35:19 +0200

    Slit databasen

---
+++ Slit database.sql
@@ -0,0 +1,21 @@
+create database Slit;
+
+create table userAccount(
+	id int auto_increment primary key,
+    firstName varchar(25),
+    lastName varchar(25),
+    email varchar(40),
+    pass varchar(30)
+);
+
+
+create table module(
+	id int primary key,
+    title varchar(75),
+    description varchar(250),
+    goals varchar(250),
+    resources varchar(250),
+    task varchar(250),
+    deadline date
+);
+
