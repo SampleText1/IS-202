cf3bf1ebf8cb9914236fc205cac416e96cca6eca
Vegard Steinnes <vegars14@uia.no>
2017-11-08 13:33:55 +0100

    auth try 2

--- Slit-v1/src/java/Loginsystem/Login.java
+++ Slit-v1/src/java/Loginsystem/Login.java
@@ -3,10 +3,6 @@ package Loginsystem;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-<<<<<<< HEAD
-import java.sql.*;
-=======
->>>>>>> Vegard
 import javax.servlet.annotation.WebServlet;
 
 @WebServlet(name = "login", urlPatterns = {"/login"})
@@ -20,16 +16,12 @@ public class Login extends HttpServlet {
         String email = request.getParameter("email");
         String pass = request.getParameter("pass");
         
-<<<<<<< HEAD
-        if(Validate.checkUser(email, pass))
-=======
         if(Validate.checkAdmin(email, pass))
         {
             RequestDispatcher rs = request.getRequestDispatcher("ansatt.html");
             rs.forward(request, response);
         }
         else if(Validate.checkUser(email, pass))
->>>>>>> Vegard
         {
             RequestDispatcher rs = request.getRequestDispatcher("student.html");
             rs.forward(request, response);
@@ -37,11 +29,7 @@ public class Login extends HttpServlet {
         else
         {
            out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">");
-<<<<<<< HEAD
-           out.println("Email eller passord er feil.");
-=======
            out.println("<div class=alert>Feil email eller passord!</div>");
->>>>>>> Vegard
            RequestDispatcher rs = request.getRequestDispatcher("index.html");
            rs.include(request, response);
         }
