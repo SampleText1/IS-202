f25e82e826b855c880aa1b49217d88eb7b20f918
Vegard Steinnes <vegars14@uia.no>
2017-11-08 13:28:51 +0100

    auth

--- Slit-v1/src/java/Loginsystem/Login.java
+++ Slit-v1/src/java/Loginsystem/Login.java
@@ -3,7 +3,10 @@ package Loginsystem;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+<<<<<<< HEAD
 import java.sql.*;
+=======
+>>>>>>> Vegard
 import javax.servlet.annotation.WebServlet;
 
 @WebServlet(name = "login", urlPatterns = {"/login"})
@@ -17,7 +20,16 @@ public class Login extends HttpServlet {
         String email = request.getParameter("email");
         String pass = request.getParameter("pass");
         
+<<<<<<< HEAD
         if(Validate.checkUser(email, pass))
+=======
+        if(Validate.checkAdmin(email, pass))
+        {
+            RequestDispatcher rs = request.getRequestDispatcher("ansatt.html");
+            rs.forward(request, response);
+        }
+        else if(Validate.checkUser(email, pass))
+>>>>>>> Vegard
         {
             RequestDispatcher rs = request.getRequestDispatcher("student.html");
             rs.forward(request, response);
@@ -25,7 +37,11 @@ public class Login extends HttpServlet {
         else
         {
            out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">");
+<<<<<<< HEAD
            out.println("Email eller passord er feil.");
+=======
+           out.println("<div class=alert>Feil email eller passord!</div>");
+>>>>>>> Vegard
            RequestDispatcher rs = request.getRequestDispatcher("index.html");
            rs.include(request, response);
         }
--- Slit-v1/src/java/Loginsystem/Validate.java
+++ Slit-v1/src/java/Loginsystem/Validate.java
@@ -4,17 +4,51 @@ import java.sql.*;
 
 public class Validate
  {
+<<<<<<< HEAD
+=======
+    public static boolean checkAdmin(String email,String pass) 
+     {
+      boolean st =false;
+      try{
+
+	 //loading drivers for mysql
+         Class.forName("org.mariadb.jdbc.Driver");
+
+ 	 //creating connection with the database 
+         Connection con=DriverManager.getConnection
+                        ("jdbc:mariadb://localhost:3306/slit","Vegard", "");
+         PreparedStatement ps =con.prepareStatement
+                             ("select * from userAccount where email=? and pass=? and admin=1");
+         ps.setString(1, email);
+         ps.setString(2, pass);
+         ResultSet rs =ps.executeQuery();
+         st = rs.next();
+        
+      }catch(ClassNotFoundException | SQLException e)
+      {
+      }
+         return st;                 
+  } 
+>>>>>>> Vegard
      public static boolean checkUser(String email,String pass) 
      {
       boolean st =false;
       try{
 
 	 //loading drivers for mysql
+<<<<<<< HEAD
          Class.forName("com.mysql.jdbc.Driver");
 
  	 //creating connection with the database 
          Connection con=DriverManager.getConnection
                         ("jdbc:mysql://localhost:3306/slit","Vegard", "gunners14");
+=======
+         Class.forName("org.mariadb.jdbc.Driver");
+
+ 	 //creating connection with the database 
+         Connection con=DriverManager.getConnection
+                        ("jdbc:mariadb://localhost:3306/slit","Vegard", "");
+>>>>>>> Vegard
          PreparedStatement ps =con.prepareStatement
                              ("select * from userAccount where email=? and pass=?");
          ps.setString(1, email);
@@ -22,9 +56,14 @@ public class Validate
          ResultSet rs =ps.executeQuery();
          st = rs.next();
         
+<<<<<<< HEAD
       }catch(Exception e)
       {
           e.printStackTrace();
+=======
+      }catch(ClassNotFoundException | SQLException e)
+      {
+>>>>>>> Vegard
       }
          return st;                 
   }   
