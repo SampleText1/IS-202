241b462f37f36fac440d6758a6efe46bb1c3073d
Vegard <vegars14@uia.no>
2017-12-08 13:41:43 +0100

    Fikset DbConnection til Validate/Login.
    Må fikses på ModuleMethods, StudentMethods etc.

--- Slit-v1/src/java/Loginsystem/Validate.java
+++ Slit-v1/src/java/Loginsystem/Validate.java
@@ -1,53 +1,47 @@
 package Loginsystem;
 
 import java.sql.*;
+import lib.DbConnection;
 
-public class Validate
- {
-    public static boolean checkAdmin(String email,String pass) 
+public class Validate {
+    
+   DbConnection dbCode = new DbConnection();
+   Connection conn = dbCode.Connect();
+    
+    public boolean checkAdmin(String email,String pass) 
      {
       boolean st =false;
       try{
-
-	 //loading drivers for mysql
-         Class.forName("org.mariadb.jdbc.Driver");
-
- 	 //creating connection with the database 
-         Connection con=DriverManager.getConnection
-                        ("jdbc:mariadb://localhost:3306/slit","root","");
-         PreparedStatement ps =con.prepareStatement
+                Statement statement = conn.createStatement();
+                
+         PreparedStatement ps =conn.prepareStatement
                              ("select * from userAccount where email=? and pass=? and admin=1");
          ps.setString(1, email);
          ps.setString(2, pass);
          ResultSet rs =ps.executeQuery();
          st = rs.next();
         
-      }catch(ClassNotFoundException | SQLException e)
+      }catch(SQLException e)
       {
       }
          return st;                 
   } 
-     public static boolean checkUser(String email,String pass) 
+     public boolean checkUser(String email,String pass) 
      {
       boolean st =false;
       try{
-
-	 //loading drivers for mysql
-         Class.forName("org.mariadb.jdbc.Driver");
-
- 	 //creating connection with the database 
-         Connection con=DriverManager.getConnection
-                        ("jdbc:mariadb://localhost:3306/slit","root","");
-         PreparedStatement ps =con.prepareStatement
+                Statement statement = conn.createStatement();
+                
+         PreparedStatement ps =conn.prepareStatement
                              ("select * from userAccount where email=? and pass=?");
          ps.setString(1, email);
          ps.setString(2, pass);
          ResultSet rs =ps.executeQuery();
          st = rs.next();
         
-      }catch(ClassNotFoundException | SQLException e)
+      }catch(SQLException e)
       {
       }
          return st;                 
-  }   
+  }  
 }
